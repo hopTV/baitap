a8355be738f1d32809f0cf9c4aeb80d37e5aa7c6
hop tran <hoptran270399@gmail.com>
2022-03-10 15:37:44 +0700

    buổi 2

--- react.buoi1/index.js
+++ react.buoi1/index.js
@@ -1,32 +1,26 @@
 const root = document.getElementById("root");
-
 const Member = (props) => {
-  const { name, age, handleTranfer, renderExtend, handleEdit, user, index } =
-    props;
+  const { name, age, handleTranfer, renderExtend } = props;
   return (
     <div>
       <span>name: {name}</span> -<span>age: {age}</span>
       <button onClick={() => handleTranfer()}>tranfer</button>
-      <button onClick={() => handleEdit(user, index)}>edit</button>
       {renderExtend && renderExtend()}
     </div>
   );
 };
-
-const INIT_DATA = [
-  {
-    name: "",
-    age: "",
-    classType: "react",
-  },
-  {},
-];
+const INIT_DATA = {
+  name: "",
+  age: "",
+  classType: "react",
+};
+// window.localStorage.clear();
 const TranferMember = () => {
-  const [edit, setEdit] = React.useState(false);
-  // const [id, setId] = React.useState();
+  const [input, setInput] = React.useState("");
   const [reactMembers, setReactMember] = React.useState(() => {
     const members = JSON.parse(localStorage.getItem("members"));
-    console.log(members);
+    // members = localStorage.removeItem(javaMembers);
+
     if (!members || !members.reactMembers) {
       return [];
     }
@@ -41,6 +35,7 @@ const TranferMember = () => {
     }
     return members.javaMembers;
   });
+
   const saveData = () => {
     localStorage.setItem(
       "members",
@@ -82,7 +77,6 @@ const TranferMember = () => {
     setReactMember([...reactMembers]);
     setJavaMember([...javaMembers]);
   };
-
   const [formData, setFormData] = React.useState(INIT_DATA);
   const handleInput = (e) => {
     const value =
@@ -102,117 +96,121 @@ const TranferMember = () => {
     }
     setFormData(INIT_DATA);
   };
+  const getMember = JSON.parse(localStorage.getItem("members"));
+  console.log(">>>check member: ", getMember);
 
-  const handleEdit = (user, index) => {
-    
-    setEdit(true);
-    // setId(index);
-    // let isCheck = Object.key()
-    console.log(">>>check user", user);
+  let joinMember = [...javaMembers, ...reactMembers];
+  // console.log(">>> chek new name:", getName);
+
+  const handeOnChangInput = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
   };
+
+  if (input.length > 0) {
+    joinMember = joinMember.filter((i) => {
+      return i.name.match(input);
+    });
+  }
+
+  // const handleSearch = (i) => {
+  //   // joinMember = joinMember.filter((i) => {
+  //   //   return i.name.match(input);
+  //   // });
+  //   // return joinMember
+  // };
+  // console.log(">> search:", joinMember);
+
+  const [sortAge, setSortAge] = React.useState(javaMembers);
+  const handleSortByAge = () => {
+    const sortBy = sortAge.sort((a, b) => {
+      return b.age - a.age;
+    });
+    setSortAge(sortBy);
+    console.log(">>> check sort:", sortBy);
+  };
+
   return (
     <div>
       <h1>list member of React class</h1>
-      {reactMembers.length > 0
-        ? reactMembers.map((user, index) => {
-            return (
-              <Member
-                name={user.name}
-                age={user.age}
-                key={index}
-                handleTranfer={() => tranferReactToJavaMember(index)}
-                handleEdit={() => handleEdit(user, index)}
-                //   renderExtend={() => <span>hello by react</span>
-              />
-            );
-          })
+      {reactMembers.length > 0 && joinMember && sortAge
+        ? reactMembers
+            .filter((i) => {
+              return i.name.match(input);
+            })
+            .map((user, index) => {
+              return (
+                <Member
+                  name={user.name}
+                  age={user.age}
+                  key={index}
+                  handleTranfer={() => tranferReactToJavaMember(index)}
+                  //   renderExtend={() => <span>hello by react</span>
+                />
+              );
+            })
         : "empty class"}
       <h1>list member of Java class</h1>
-      {javaMembers.length > 0
-        ? javaMembers.map((user, index) => {
-            return (
-              <Member
-                name={user.name}
-                age={user.age}
-                key={index}
-                handleTranfer={() => tranferJavaToReactMember(index)}
-                handleEdit={() => handleEdit(user, index)}
-                //   renderExtend={() => <span>hello by java</span>}/>
-              />
-            );
-          })
+      {javaMembers.length > 0 && joinMember && sortAge
+        ? javaMembers
+            .filter((i) => {
+              return i.name.match(input);
+            })
+            .map((user, index) => {
+              return (
+                <Member
+                  name={user.name}
+                  age={user.age}
+                  key={index}
+                  handleTranfer={() => tranferJavaToReactMember(index)}
+                  //   renderExtend={() => <span>hello by java</span>}/>
+                />
+              );
+            })
         : "empty class"}
       <h1>add member</h1>
-      <div>
-        {!edit ? (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              // e.preventDefault() dùng để bỏ qua sự kiện mặc định của event của 1 thẻ html nào đó như thẻ form, a
-              handleSubmit();
-            }}
-            // action="/api"
-            // method="post"
-          >
-            <label>name</label>
-            <input
-              name="name"
-              value={formData.name}
-              onChange={(e) => handleInput(e)}
-            ></input>
-            {" --- "}
-            <label>age</label>
-            <input
-              value={formData.age}
-              name="age"
-              onChange={(e) => handleInput(e)}
-            ></input>
-            <select
-              name="classType"
-              onChange={(e) => handleInput(e)}
-              value={formData.classType}
-            >
-              <option value="react">React</option>
-              <option value="java">Java</option>
-            </select>
-            <button>submit</button>
-            {/* checkme<input type="checkbox" name="testCheckbox" /> */}
-          </form>
-        ) : (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              // e.preventDefault() dùng để bỏ qua sự kiện mặc định của event của 1 thẻ html nào đó như thẻ form, a
-              handleSubmit();
-            }}
-            // action="/api"
-            // method="post"
-          >
-            <label>name</label>
-            <input
-              name="name"
-              value={formData.name.value}
-              onChange={(e) => handleInputEdit(e)}
-            ></input>
-            {" --- "}
-            <label>age</label>
-            <input
-              value={formData.age.value}
-              name="age"
-              onChange={(e) => handleInputEdit(e)}
-            ></input>
-            <select
-              name="classType"
-              onChange={(e) => handleInputEdit(e)}
-              value={formData.classType}
-            >
-              <option value="react">React</option>
-              <option value="java">Java</option>
-            </select>
-            <button>cập nhật</button>
-            {/* checkme<input type="checkbox" name="testCheckbox" /> */}
-          </form>
-        )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // e.preventDefault() dùng để bỏ qua sự kiện mặc định của event của 1 thẻ html nào đó như thẻ form, a
+          handleSubmit();
+        }}
+        // action="/api"
+        // method="post"
+      >
+        <label>name</label>
+        <input
+          name="name"
+          value={formData.name}
+          onChange={(e) => handleInput(e)}
+        ></input>
+        {" --- "}
+        <label>age</label>
+        <input
+          value={formData.age}
+          name="age"
+          onChange={(e) => handleInput(e)}
+        ></input>
+        <select
+          name="classType"
+          onChange={(e) => handleInput(e)}
+          value={formData.classType}
+        >
+          <option value="react">React</option>
+          <option value="java">Java</option>
+        </select>
+        <button>submit</button>
+        {/* checkme<input type="checkbox" name="testCheckbox" /> */}
+      </form>
+      <div style={{ margin: "10px" }}>
+        <input
+          type="text"
+          placeholder="search"
+          value={input}
+          onChange={(e) => handeOnChangInput(e)}
+        />
+        <button>search</button>
+        <button onClick={handleSortByAge}>sort by</button>
       </div>
     </div>
   );
